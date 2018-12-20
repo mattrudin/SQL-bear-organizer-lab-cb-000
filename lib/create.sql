da613ee63288092e37f75b6d3c40e5117b566cc9
Matthias Rudin <matthias.rudin@live.com>
2018-12-20 05:13:09 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
-CREATE TABLE cats (
+CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
   gender TEXT,
-  color TEXT, 
+  color TEXT,
   temperament TEXT,
   alive TEXT
 );
